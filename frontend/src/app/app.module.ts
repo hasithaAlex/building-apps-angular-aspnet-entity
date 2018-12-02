dda9862a8436c0c3d135be1ec1825244e9f703fd
hasitha <hasithamadusanka90@gmail.com>
2018-12-03 04:33:52 +0530

    setup router

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 import { MdButtonModule, MdInputModule, MdCardModule, MdListModule } from '@angular/material'
 
-import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component'
 
 import { QuestionComponent } from './question.component'
 import { QuestionsComponent } from './questions.component';
 import { ApiService } from './api.service'
+
+const routes = [
+  {path: 'question', component: QuestionComponent},
+  {path: 'questions', component: QuestionsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,6 +24,7 @@ import { ApiService } from './api.service'
   imports: [
     BrowserModule, 
     HttpClientModule,
+    RouterModule.forRoot(routes),
     FormsModule,
     BrowserAnimationsModule, 
     MdButtonModule, 
